93f6d912ebb109702f95148b87ff879e9615069c
Yago Toro molina <ytoro-mo@c1r4s2.42malaga.com>
2022-04-28 16:58:49 +0200

    Comprobaciones

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,15 @@
 /*   By: ytoro-mo <ytoro-mo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 09:51:47 by ytoro-mo          #+#    #+#             */
-/*   Updated: 2022/04/28 13:31:05 by ytoro-mo         ###   ########.fr       */
+/*   Updated: 2022/04/28 16:56:31 by ytoro-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
 
 char	*ft_get_all_lines(int fd, char *all_txt);
 char	*ft_get_line(char *all_txt);
@@ -26,8 +30,11 @@ char	*get_next_line(int fd)
 	all_txt = ft_get_all_lines(fd, all_txt);
 	if (!all_txt)
 		return (NULL);
+	printf("LINEA PRIMERA: %s\n", all_txt);
 	line = ft_get_line(all_txt);
+	printf("LINEA QUE QUIERO: %s\n", line);
 	all_txt = ft_new_previous_line(all_txt);
+	printf("LINEA SIGUIENTE: %s\n", all_txt);
 	return (line);
 }
 
@@ -106,3 +113,33 @@ char	*ft_new_previous_line(char	*all_txt)
 	free(all_txt);
 	return (previous);
 }
+
+/*int	main(void)
+{
+	char	*line;
+	int		i;
+	int		fd1;
+	int		fd2;
+	int		fd3;
+	fd1 = open("tests/test.txt", O_RDONLY);
+	fd2 = open("tests/test2.txt", O_RDONLY);
+	fd3 = open("tests/test3.txt", O_RDONLY);
+	i = 1;
+	while (i < 7)
+	{
+		line = get_next_line(fd1);
+		//printf("line [%02d]: %s", i, line);
+		free(line);
+		line = get_next_line(fd2);
+		//printf("line [%02d]: %s", i, line);
+		free(line);
+		line = get_next_line(fd3);
+		//printf("line [%02d]: %s", i, line);
+		free(line);
+		i++;
+	}
+	close(fd1);
+	close(fd2);
+	close(fd3);
+	return (0);
+}*/
